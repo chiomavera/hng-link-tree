ddef771fe8e07c3609fc2a5074c0a4791468ad8c
chiomavera <you@example.com>
2022-11-04 03:20:27 +0100

    ADD: contact page created

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,11 @@
+const contact = () => {
+    return (
+        <main>
+            <header>
+                <h1>Contact Me!</h1>
+            </header>
+        </main>
+    )
+}
+
+export default contact
