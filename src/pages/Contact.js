bb9e4133db2450cd92e3252c040b8e6f72c4a495
chiomavera <you@example.com>
2022-11-04 05:50:32 +0100

    ADD: create contact page

---
+++ src/pages/Contact.js
@@ -0,0 +1,26 @@
+const contact = () => {
+    return (
+        <main id="contact-container">
+            <header className="contact__header">
+                <h1 className="contact__head">Contact Me</h1>
+                <p className="head__description">Hi there, contact me to ask me about everything you have in mind</p>
+            </header>
+
+            <section className="contact__body">
+                <form id="form">
+                  <div className="form__fields">
+                    <div className="form__control">
+                        
+                    </div>
+                    <div className="form__control"></div>
+                    <div className="form__control"></div>
+                  </div>
+
+                </form>
+            </section>
+        
+        </main>
+    );
+};
+
+export default contact
