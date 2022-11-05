5507e9d08d0a083868bdd9af7ca10bc7b0bae9a1
chiomavera <you@example.com>
2022-11-05 06:53:09 +0100

    MAKE: structure contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,26 +1,72 @@
 const contact = () => {
-    return (
-        <main id="contact-container">
-            <header className="contact__header">
-                <h1 className="contact__head">Contact Me</h1>
-                <p className="head__description">Hi there, contact me to ask me about everything you have in mind</p>
-            </header>
+  const name = "Chioma Nkanmuo";
 
-            <section className="contact__body">
-                <form id="form">
-                  <div className="form__fields">
-                    <div className="form__control">
-                        
-                    </div>
-                    <div className="form__control"></div>
-                    <div className="form__control"></div>
-                  </div>
+  const formSubmit = (e) => {
+    alert('sent');
+    e.preventDefault();
+  }
 
-                </form>
-            </section>
-        
-        </main>
-    );
+  return (
+    <section class="container">
+      <header className="header">
+        <h1 className="heading">Contact Me</h1>
+        <p className="description">
+          Hi there, contact me to ask me about everything you have in mind
+        </p>
+      </header>
+
+      {/* form section */}
+         <div className="form__section">
+         <form onSubmit={formSubmit} id="form">
+              <div className="form__control">
+                <label for="first_name">First name</label>
+                <input
+                  type="text"
+                  id="first_name"
+                  placeholder="Enter your first name"
+                  required
+                />
+              </div>
+              <div className="form__control">
+                <label for="last_name">Last name</label>
+                <input
+                  type="text"
+                  id="last_name"
+                  placeholder="Enter your last name"
+                  required
+                />
+              </div>
+              <div className="form__control">
+                <label for="email">Email</label>
+                <input
+                  type="email"
+                  id="email"
+                  placeholder="yourname@email.com"
+                  required
+                />
+              </div>
+              <div className="form__control message">
+                <label for="message">Message</label>
+                <textarea
+                  id="message"
+                  placeholder="Send me a message and I'll reply as soon as possible..."
+                  required
+                />
+                <small className="error">please enter message</small>
+              </div>
+              <div className="check-box">
+                <input type="checkbox" id="checkbox"/>
+                  <label for="checkbox" className="agree">
+                    You agree to providing your data to {name} who may contact
+                    you.
+                  </label>
+               
+              </div>
+            <button type="submit" id="btn__submit">Send message</button>
+          </form>
+         </div>
+    </section>
+  );
 };
 
-export default contact
+export default contact;
